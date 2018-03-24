396d722807f336d12e69b85bd62428eb0f513a38
Adriano A Costa <sdcomputadores@gmail.com>
2018-03-24 08:43:39 -0300

    - Encripitacao da senha com bcrypt; - Omitindo a senha na consulta;

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 class User extends Eloquent
 {
     protected $fillable = ['name','email','password'];
+    protected $hidden = ['password'];
+    
+    public function setPasswordAttribute($value)
+    {
+        if(!empty($value)){
+            $this->attributes['password'] = password_hash($value, PASSWORD_DEFAULT);
+        }
+    }
 }
